bac7187ef8bb3f72921b200141aab48b7e6678a6
Kvartti <a1704516@myy.haaga-helia.fi>
2021-05-08 15:28:45 +0300

    deleted some comments

--- src/views/Pairings.vue
+++ src/views/Pairings.vue
@@ -4,16 +4,6 @@
             <router-link :to="{ name: 'Second', params: { id: this.$route.params.name }}" tag="button" class="backbutton">Back</router-link><br/>
        
     <router-link :to="{ name: 'Second', params: { id: value.code }}" tag="button" v-for="(value, index) in this.$store.getters.characters" v-bind:key="index">{{value.name}}</router-link>
-        
-  <!--      <router-link :to="{ name: 'Second', params: { id: 'annette' }}" tag="button">Annette</router-link>
-        <router-link :to="{ name: 'Second', params: { id: 'byleth_f' }}" tag="button">Byleth (F)</router-link>
-        <router-link :to="{ name: 'Second', params: { id: 'constance' }}" tag="button">Constance</router-link>
-        <router-link :to="{ name: 'Second', params: { id: 'dimitri' }}" tag="button">Dimitri</router-link>
-        <router-link :to="{ name: 'Second', params: { id: 'dorothea' }}" tag="button">Dorothea</router-link>
-        <router-link :to="{ name: 'Second', params: { id: 'felix' }}" tag="button">Felix</router-link>
-        <router-link :to="{ name: 'Second', params: { id: 'ferdinand' }}" tag="button">Ferdinand</router-link>
-        <router-link :to="{ name: 'Second', params: { id: 'yuri' }}" tag="button">Yuri</router-link> -->
-      
       <br/>
       <br/>
     <router-link :to="{ name: 'AddMore'}" tag="button" class="addButton">Add More</router-link>
